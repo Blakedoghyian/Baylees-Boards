00de605d95f97938545a8531702ba07bb235fca2
Jessica Hille <jessicalhille@gmail.com>
2022-03-22 23:18:27 -0500

    photos and text updated

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -43,8 +43,7 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          Baylee's Boards
         </Link>
       </h1>
 
